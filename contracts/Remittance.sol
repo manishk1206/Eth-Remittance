f63408544f8c1505b4340394dff9e5bf6b29280c
Manish Kumar <44619042+manishk1206@users.noreply.github.com>
2019-05-19 00:27:17 +0530

    Madec hanges as per Adel's 2nd review comments

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -18,9 +18,7 @@ contract Remittance is Pausable {
     struct remit{
         uint amount;
         address sender;
-        address receiver;
         uint expiry;
-        bytes32 hashedPwd;
     }
 
     mapping (bytes32 => remit) remitmap ; // stores all the remittances with key as 2-factor OTP hash
@@ -33,53 +31,46 @@ contract Remittance is Pausable {
     }
 
     // Alice can deposit
-    function sendRemit(bytes32 _hashOrig, uint _deadline, address _receiver ) public payable onlyIfRunning {
+    function sendRemit(bytes32 _hashedPwd, uint _deadline ) public payable onlyIfRunning {
+
+        //Restricting use of duplicate passwords
+        require (remitmap[_hashedPwd].sender == address(0),"Password already in use.");
 
         require (msg.value > 0, "There should be some non-zero value to split" );
-        require(_deadline < maxTimeLimit,"Please lower the expiry and try again" );
-
-        // adding salt to the original hash
-        bytes32 saltedHash = keccak256(abi.encodePacked(_hashOrig,address(this)));
+        require(_deadline < maxTimeLimit,"Please lower the expiry time and try again" );
 
         remit.amount = msg.value;
         remit.sender = msg.sender;
-        remit.receiver = _receiver; // A hacker may know the hash, but he can't impersonate receiver
-        remit.expiry = now + _deadline;
-        remit.hashedPwd = saltedHash;
+        remit.expiry = now.add(_deadline);
 
-        remitmap[_hashOrig] = remit; // mapping to original hashed Password
+        remitmap[_hashedPwd] = remit; // mapping to original hashed Password
         emit LogSendFunds(msg.sender,msg.value,_deadline);
 
      }
 
     // Carol can withdraw
-    function withdrawRemit(bytes32 _hashOrig) public onlyIfRunning {
+    function withdrawRemit(bytes32 _smsOtp) public onlyIfRunning {
 
-        // This is done to imply that ONLY this contract address is applicable for this particularRemittance
-        // So, Even if the hacker knows _hashOrig, he cannot succed as his contract address will be different
-        require (keccak256(abi.encodePacked(_hashOrig,address(this))) == remitmap[_hashOrig].hashedPwd,"Passwords mismatch, Withdrawal denied!");
+       bytes32 hashedPwd =  this.getOtpHash (_smsOtp ,msg.sender);
+     //   require( now < remitmap[hashedPwd].expiry,"Sorry pal! Claiming Period Over. Call our office");
 
-        require (msg.sender == remitmap[_hashOrig].receiver,"Hey petty thief..You shall be punished!");
-        require( now < remitmap[_hashOrig].expiry,"Sorry pal! Claiming Period Over. Call our office");
-
-        uint balance = remitmap[_hashOrig].amount;
+        uint balance = remitmap[hashedPwd].amount;
         require(balance > 0, "Balance zero. Either already Remitted or claimed");
-        remitmap[_hashOrig].amount = 0;
+        remitmap[hashedPwd].amount = 0;
         emit LogWithdrawnFunds(msg.sender,balance);
         msg.sender.transfer(balance);
 
     }
 
     // Alice can claim back after cutoff time has passed
-    function claimBackRemit(bytes32 _hashOrig) public onlyIfRunning {
+    function claimBackRemit(bytes32 _hashedPwd) public onlyIfRunning {
 
-        require (keccak256(abi.encodePacked(_hashOrig,address(this))) == remitmap[_hashOrig].hashedPwd,"Passwords mismatch, Claimback denied!");
-        require (msg.sender == remitmap[_hashOrig].sender,"You ain't the owner, Better luck next time :)");
-        require( now > remitmap[_hashOrig].expiry,"Don't be too greedy, have patience!");
+        require (msg.sender == remitmap[_hashedPwd].sender,"You ain't the owner, Better luck next time :)");
+        require( now > remitmap[_hashedPwd].expiry,"Don't be too greedy, the cutoff time is not reached!");
 
-        uint balance = remitmap[_hashOrig].amount;
+        uint balance = remitmap[_hashedPwd].amount;
         require(balance > 0, "Balance zero. Either already Remitted or claimed");
-        remitmap[_hashOrig].amount = 0;
+        remitmap[_hashedPwd].amount = 0;
         emit LogClaimedBackFunds(msg.sender,balance);
         msg.sender.transfer(balance);
 
@@ -87,7 +78,7 @@ contract Remittance is Pausable {
 
     // get the hash using Bob's sms and Carol's address, done off-chain
     function getOtpHash (bytes32 _smsOtp ,address _receiver) pure external returns(bytes32 hash){
-        return keccak256(abi.encodePacked(_smsOtp, _receiver));
+        return keccak256(abi.encodePacked(_smsOtp, _receiver,address(this)));
     }
 
     //fall-back function

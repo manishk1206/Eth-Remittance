631f758e74a4e02cb0c5d19f2193e0f28377483a
Manish Kumar <44619042+manishk1206@users.noreply.github.com>
2019-05-19 11:20:07 +0530

    Passed parameter values for constructor

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 const Remittance = artifacts.require("Remittance");
 
 module.exports = function(deployer) {
-  deployer.deploy(Remittance);
+  deployer.deploy(Remittance, 604800, true); // _maxTime = 7 days = 604800 seconds, _state = true i.e running
 };

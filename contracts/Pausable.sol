eb2e21e52f50b8d427dcdb90ffd11bafad20015a
Manish Kumar <44619042+manishk1206@users.noreply.github.com>
2019-05-22 21:48:31 +0530

    Changed variable name and initialisation place

--- contracts/Pausable.sol
+++ contracts/Pausable.sol
@@ -4,11 +4,11 @@ import "./Ownable.sol";
 
 contract Pausable is Ownable{
     bool private isRunning;
-    bool private depositAllowed = true;
+    bool private isWindDown;
     
     event LogPausedContract(address sender);
     event LogResumedContract(address sender);
-    event LogPausedDeposit(address sender);
+    event LogWindDown(address sender);
     
     modifier onlyIfRunning {
         require(isRunning);
@@ -21,40 +21,45 @@ contract Pausable is Ownable{
     }
     
     modifier onlyIfAllowed {
-        require(depositAllowed);
+        require(!isWindDown);
         _;
     }
     
     constructor(bool _initialState) public{
         isRunning = _initialState;
+        isWindDown = false;
     }
 
     function getIsRunning() public view returns(bool){
         return isRunning;
     }
     
-    function getDepositAllowed() public view returns(bool){
-        return depositAllowed;
+    function getIsWindDown() public view returns(bool){
+        return isWindDown;
     }
     
+    //can be used to pause a contract temporarily or as needed
     function pauseContract() public onlyOwner onlyIfRunning returns (bool success){
         isRunning = false;
         emit LogPausedContract(msg.sender);
         return true;
     }
     
+    //can be used to resume a contract back live from pausd state
     function resumeContract() public onlyOwner onlyIfPaused returns (bool success){
         isRunning = true;
         emit LogResumedContract(msg.sender);
         return true;
     }
     
-    function pauseDeposit() public onlyOwner returns (bool success){
-        depositAllowed = false;
-        emit LogPausedDeposit(msg.sender);
+    //can be used for something like pausing deposit while allowing only withdraw
+    function windDown() public onlyOwner returns (bool success){
+        isWindDown = true;
+        emit LogWindDown(msg.sender);
         return true;
     }
     
+    //delete or remove the contract if the need be
     function kill() external onlyOwner onlyIfPaused {
         selfdestruct(msg.sender); 
     }
